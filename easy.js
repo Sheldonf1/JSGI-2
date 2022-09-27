ffeb08b63c050abb347e56e2ad44c24c12e0c1a5
Sheldon Ferguson <sheldonferguson@AdminisatorsMBP.lan>
2022-09-26 21:46:57 -0400

    Edited

--- easy.js
+++ easy.js
@@ -1,9 +1,9 @@
 let students =
-    [{ fName: "john", lName: "doe", age: 12 },
-    { fName: "josh", lName: "dash", age: 16 },
-    { fName: "jobs", lName: "steve", age: 20 }]
+    [{ fName: "Sheldon", lName: "Ferguson", age: 12 },
+    { fName: "John", lName: "Doe", age: 16 },
+    { fName: "Candice", lName: "Lewis", age: 20 }]
 
-let greeting = `hello, my name is 
-${students[1].fName} ${students[1].lName} and im 
-${students[1].age} years old`
+let greeting = `Hello, my name is 
+${students[1].fName} ${students[1].lName} and i'm 
+${students[1].age} years old.`
 console.log(greeting)
